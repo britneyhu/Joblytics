f75dd58b5659e276dfe038503c2f34baafe8c1d1
britneyhu <britneyhu2@gmail.com>
2025-05-29 17:51:06 -0700

    started header of home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './styles/global.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="home-body">
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    
+    return(
+        <div className="header">
+            Joblytics
+        </div>
+    );
+}
+
+export default Header;
