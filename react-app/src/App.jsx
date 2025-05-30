821be084573ecf8664f19a5fa99c64583fc2356b
britneyhu <britneyhu2@gmail.com>
2025-05-30 13:29:56 -0700

    added functionality to dropdown menu

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,27 @@
+import React, {useState, useEffect} from "react";
+
 import './styles/global.css';
 import Header from './components/Header';
 import Graph from './components/Graph';
 import Dropdown from './components/Dropdown';
 
 function App() {
+  const [graphOption, setGraphOption] = useState("ALL");
+
+  const updateGraphOption = (option) =>{
+    setGraphOption(option);
+  }
+
+  useEffect(()=>{
+    console.log(graphOption);
+  }, [graphOption])
+
   return (
     <div className="home-body">
       <Header></Header>
       <div className="content-title">JASON'S ANALYTICS</div>
       <Graph></Graph>
-      <Dropdown></Dropdown>
+      <Dropdown className="graph-dropdown" options={["ALL", "APPLIED", "RESPONDED", "REJECTED"]} currentOption={graphOption} updateOption={updateGraphOption}></Dropdown>
     </div>
   );
 }
--- react-app/src/components/Dropdown.jsx
+++ react-app/src/components/Dropdown.jsx
@@ -1,8 +1,31 @@
-function Dropdown(){
+import React, { useState } from 'react';
+
+function Dropdown({options, currentOption, updateOption}){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = ()=> setIsOpen(prev => !prev);
+
+    const changeOption = (option)=>{
+        updateOption(option);
+        setIsOpen(false);
+    }
     
     return(
         <div className="dropdown">
-            <i class="chevron-down fa-solid fa-chevron-down"></i>
+            <button className="dropdown-button" onClick={toggleDropdown}>
+                <div className="current-option">{currentOption}</div>
+                <i className="chevron-down fa-solid fa-chevron-down"></i>
+            </button>
+
+            {isOpen && (
+                <ul className="dropdown-menu">
+                    {options.map((option, index) => (
+                        <li className="dropdown-option" key={index} onClick={()=> changeOption(option)}>
+                            <div className="dropdown-option-text">{option}</div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
