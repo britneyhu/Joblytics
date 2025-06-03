281f09cdc0fcb7d2b5b077e2db1d07ac99eb4b77
britneyhu <britneyhu2@gmail.com>
2025-06-02 17:25:50 -0700

    changed dropdown on home page to be a <select>

--- react-app/src/AppIndex.jsx
+++ react-app/src/AppIndex.jsx
@@ -3,17 +3,16 @@ import React, {useState, useEffect} from "react";
 import './styles/global.css';
 import Header from './components/Header';
 import Graph from './components/Graph';
-import Dropdown from './components/Dropdown';
 import Stats from './components/Stats';
 import AddAppButton from './components/AddAppButton';
 import NavButton from './components/NavButton';
 
 function AppIndex() {
-  const [graphOption, setGraphOption] = useState("ALL");
+  const [graphOption, setGraphOption] = useState("all");
   const [stats, setStats] = useState({"applied": 0, "responded": 0, "rejected": 0, "interviewing": 0, "offer": 0})
 
-  const updateGraphOption = (option) =>{
-    setGraphOption(option);
+  const updateGraphOption = (event) =>{
+    setGraphOption(event.target.value);
   }
 
   const redirectToAddApp =()=>{
@@ -34,7 +33,14 @@ function AppIndex() {
           
           <div className="content-title">JASON'S ANALYTICS</div>
           <Graph></Graph>
-          <Dropdown className="graph-dropdown" options={["ALL", "APPLIED", "RESPONDED", "REJECTED"]} currentOption={graphOption} updateOption={updateGraphOption}></Dropdown>
+          <div className="dropdown">
+            <select className="dropdown-graph" name="graph-dropdown" value={graphOption} onChange={updateGraphOption}>
+                <option value="all">ALL</option>
+                <option value="applied">APPLIED</option>
+                <option value="responded">RESPONDED</option>
+                <option value="rejected">REJECTED</option>
+            </select>
+          </div>
         </div>
 
         <div className="col2">
